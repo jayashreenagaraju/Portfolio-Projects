d14b67bbb44ee8395db9b8213fce02ea34bc90ef
jayashreenagaraju <106110595+jayashreenagaraju@users.noreply.github.com>
2023-10-27 16:43:57 +0200

    answer questiosn for Awesome chocolate project

--- Awesome chocolate Basic.sql
+++ Awesome chocolate Basic.sql
@@ -79,3 +79,69 @@ where s.Amount < 500
 and p.Team = 'Delish'
 and g.Geo in ('New Zealand','India')
 order by s.SaleDate desc;
+
+/*  1.Print details of shipments (sales) where amounts are > 2,000 and boxes are <100?*/
+select * from sales;
+
+select *
+from sales
+where Amount > 2000 and boxes < 100;
+
+/* 2. How many shipments (sales) each of the sales persons had in the month of January 2022?*/
+select p.Salesperson,count(*) as shipment_count
+from sales as s
+join people as p
+on s.SPID = p.SPID
+where SaleDate between '2022-01-01' and '2022-01-31' 
+group by Salesperson;
+
+
+/* 3. Which product sells more boxes? Milk Bars or Eclairs?*/
+select * from sales;
+select * from products;
+
+select p.product, sum(boxes) as total_boxes
+from sales as s
+join products as p
+on p.PID = s.PID
+where p.Product in ('Milk Bars','Eclairs')
+group by p.Product;
+
+/* 4. Which product sold more boxes in the first 7 days of February 2022? Milk Bars or Eclairs?*/
+select pr.product,sum(Boxes) as box_count
+from sales as s
+join products as pr
+on pr.PID = s.PID
+where pr.Product in ('Milk Bars','Eclairs') 
+and s.SaleDate between '2022-02-01' and '2022-02-07'
+group by pr.Product;
+
+
+/*5. Which shipments had under 100 customers & under 100 boxes? Did any of them occur on Wednesday?*/
+
+select * 
+from sales
+where Customers < 100 and boxes < 100;
+
+select *,
+case  when weekday(SaleDate)=2 then "wednesday"
+else ""
+end as w
+from sales
+where Customers < 100 and boxes < 100;
+
+/*6. What are the names of salespersons who had at least one shipment (sale) in the first 7 days of January 2022?*/
+
+select * from people;
+select * from sales;
+
+select distinct p.SPID,p.Salesperson,s.SaleDate
+from people as p
+join sales as s on p.SPID = s.SPID
+where s.SaleDate between '2022-01-01' and '2022-01-07';
+
+/*7.Which salespersons did not make any shipments in the first 7 days of January 2022?*/
+
+select p.SPID,p.Salesperson
+from people as p 
+where p.SPID not in(select distinct s.spid from sales as s where s.SaleDate between '2022-01-01' and '2022-01-07');
